4914e258af3a66011dfc4fadd4543933f39ccad1
Kotryos <13850665+Kotryos@users.noreply.github.com>
2026-06-26 16:24:58 +0200

    Modernize the stack with jooq and testcontainers

---
+++ src/main/java/dev/kotryos/sqlhierarchy/SqlHierarchyModelsApplication.java
@@ -0,0 +1,12 @@
+package dev.kotryos.sqlhierarchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlHierarchyModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlHierarchyModelsApplication.class, args);
+    }
+}
